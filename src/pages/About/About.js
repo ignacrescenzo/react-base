377ca0ee394c82ecb6f25d5aee0eaf244b88bc80
Ignacio Crescenzo <ignadario97@gmail.com>
2020-07-11 23:16:20 -0300

    agrego react route

---
+++ src/pages/About/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import useStyles from './styles'
+
+function About () {
+  const classes = useStyles() //eslint-disable-line
+  return <h2>About</h2>
+}
+
+export default About
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import useStyles from './styles'
+
+function Home () {
+  const classes = useStyles() // eslint-disable-line
+  return <h2>Home</h2>
+}
+
+export default Home
