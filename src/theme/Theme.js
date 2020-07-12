e315d33f543c070c958a68898c5fb43abd7ffb4d
Ignacio Crescenzo <ignadario97@gmail.com>
2020-07-11 21:51:14 -0300

    color de letra

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,13 +1,18 @@
 // https://material-ui.com/es/customization/theming/
 // https://material-ui.com/es/customization/palette/
 // https://material-ui.com/es/customization/components/#global-theme-override
-
+// https://material.io/design/color/text-legibility.html#text-backgrounds
+// https://material-ui.com/es/customization/color/
 import { createMuiTheme } from '@material-ui/core/styles'
+import grey from '@material-ui/core/colors/grey'
 
 const theme = createMuiTheme({
   palette: {
     primary: {
       main: '#8A22D2'
+    },
+    text: {
+      primary: grey[900]
     }
   },
   typography: {
