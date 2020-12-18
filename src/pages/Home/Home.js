352d34fc4663f412b08715a2a8f2d0b67bb2ab80
Ignacio Crescenzo <ignadario97@gmail.com>
2020-12-18 18:15:39 -0300

    .

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import useStyles from './styles'
-
+import test from '../../test.json'
 function Home () {
   const classes = useStyles()
+  // React.useEffect(() => {
+  //   const ar = test[0]
+  //   const array = []
+  //   // console.log(ar)
+  //   ar.filter(a => a.clasification).forEach(item => {
+  //     const code = item.clasification + item.cod
+  //     window.setInterval(() => {
+  //       window.fetch(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAoWkoyvj8ev5iHj01aOw4VNVxmJ-ZP9tI&components=postal_code:${code}|country:AR`)
+  //         .then(res => res.json())
+  //         .then(res => {
+  //           array.push(res)
+  //         })
+  //     }, 500)
+  //   })
+  //   console.log(array)
+  // }, [])
   return <h2 className={classes.title}>Home</h2>
 }
 
